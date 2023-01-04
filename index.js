3f062e2cf8d427e0eb9ba29c5985525599e1d5df
isaac <isaactonyloi@gmail.com>
2023-01-04 11:44:44 +0300

    argument binding

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ console.log(arguments);
 }
 
 getAges(23, 24,25);
+
+function sum(x = 3, y = 5) {
+
+    // return sum
+    return x + y;
+}
+
+console.log(sum(5, 15));  // 20 
+console.log(sum(7));        // 12
+console.log(sum());          // 8
